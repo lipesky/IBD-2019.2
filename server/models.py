4560bf01d3d49f5acf1eb297e5e8c1498623dd9e
Felipe Vilhena <echofelipevilhena@gmail.com>
2019-12-12 00:13:29 -0400

    ajuste no nome da coluna

--- server/models.py
+++ server/models.py
@@ -3,7 +3,7 @@ from app import db
 class Usuario(db.Model):
 	__tablename__ = 'usuario';
 
-	id = db.Column(db.Integer, primary_key = True);
+	id_usuario = db.Column(db.Integer, primary_key = True);
 	nome = db.Column(db.String(50));
 	cpf = db.Column(db.String(11), unique = True, nullable = False);
 	telefone = db.Column(db.String(13));
@@ -19,7 +19,7 @@ class Usuario(db.Model):
 
 	def serialize(self):
 		return {
-			'id'   		: self.id,
+			'id'   		: self.id_usuario,
 			'nome' 		: self.nome,
 			'cpf'		: self.cpf,
 			'telefone'  : self.telefone,
